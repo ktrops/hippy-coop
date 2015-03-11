701d15bf12fca59bec1f7144ef8d0fd16330bf33
Katie Atrops <katrops@gmail.com>
2015-03-11 11:18:22 -0700

    to get changing rooms working

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,8 @@
 class MembersController < ApplicationController
+	def home
+		@members = Member.all
+	end
+
 	def new
 		@member = Member.new
 		@member.purchases.build
@@ -11,13 +15,27 @@ class MembersController < ApplicationController
 	end
 
 	def create
+
 		@member = Member.create(member_params)
-		@room = @member.create_room
+		# @room = @member.create_room
+		@member.room.update(rent: params[:member][:room_attributes][:rent], room_name: params[:member][:room_attributes][:room_name])
+		session[:member_id] = @member.id
+		session[:room_id] = @member.room.id
 		if @member.save
 			redirect_to new_purchase_url
 		else
 			render :new
 		end
+	end
+
+	def edit
+		@member = Member.find(params[:id])
+	end
+
+	def update
+		@member = Member.find(params[:id])
+		@member.room.update(rent: member_params[:member][:room_attributes][:rent], room_name: member_params[:member][:room_attributes][:room_name])
+		redirect_to "purchases/show/:id"
 	end
 
 	def show
@@ -26,7 +44,7 @@ class MembersController < ApplicationController
 
 	def member_params
 		params.require(:member).permit(:name, :room_id,
-										room_attributes: [:rent, :name, :id, :member_id],
-										purchase_attributes: [:description, :cost, :id, :member_id])
+										room_attributes: [:rent, :room_name, :member_id],
+										purchase_attributes: [:description, :cost, :member_id])
 	end
 end
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
 	def new
 		@purchase = Purchase.new
-		@member = @purchase.member
+		@member = Member.find(session[:member_id])
 		
 	end
 
@@ -11,17 +11,35 @@ class PurchasesController < ApplicationController
 
 	def create
 		@purchase = Purchase.create(params_purch)
-		@purchase.member.build
+		@purchase.member_id = session[:member_id]
+		@purchase.save
+		@member = Member.find(session[:member_id])
+		@member.purchases.build
 		if @purchase.save
-			redirect_to room_url
+			redirect_to purchase_path(@purchase.id)
 		else
 			render :new
 		end
 	end
 
 	def show
-		@purchase = Purchase.find(params[:id])
+		# @purchases = Purchase.find(params[:id])
+		# @purchases = Purchase.where(member_id: session[:member_id])
+		@purchases = Purchase.where(member_id: params[:id])
+		
+		count = 0
+		@purchases.each do |p|
+		  count += p.cost
+		end
+		# @members = Member.all
+		# @member = Member.find_by(room_id: session[:room_id])
+		@member = Member.find(params[:id])
+		@room_rent = @member.room.rent
+		@total_rent = @room_rent - count
 	end
+
+
+
 
 	def params_purch
 		params.require(:purchase).permit(:description, :cost, 
